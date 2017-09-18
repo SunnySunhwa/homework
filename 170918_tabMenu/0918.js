847a2dd1fef0bf1f6ed614735848b2882c0f7a53
SunnySunhwa <via1229@gmail.com>
2017-09-19 00:09:10 +0900

    170918_tab menu

---
+++ 170918_tabMenu/0918.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var tab = $('.tab');
+  tab.on('click, focusin', function() {
+    $(this).parent('.tab-board').addClass('active').siblings('.tab-board').removeClass('active');
+  });
+});
