7e6beb7e92e98e997bd44e60f21bb2decbef7afd
SunnySunhwa <via1229@gmail.com>
2017-09-19 11:25:37 +0900

    jquery error modified

--- 170918_tabMenu/0918.js
+++ 170918_tabMenu/0918.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var tab = $('.tab');
-  tab.on('click, focusin', function() {
+  tab.on('click focusin', function() {
     $(this).parent('.tab-board').addClass('active').siblings('.tab-board').removeClass('active');
   });
 });
